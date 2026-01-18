c0bb41e79bb4e3e5045789ca2dba8460c6f9b6e6
achyut <achyutchaudhary47@gmail.com>
2026-01-18 22:13:59 +0530

    todo

--- src/main/java/com/example/todo/TodoController.java
+++ src/main/java/com/example/todo/TodoController.java
@@ -1,4 +1,21 @@
 package com.example.todo;
 
+import org.springframework.web.bind.annotation.RestController;
+import java.util.*;
+import java.util.ArrayList;
+
+@RestController
+//by adding this annotations in the class , spring framework will treat it as controller class
+//controller means that it is the entry point of the framework
+//controller have only two use cases collecting the request nd sending the response
+//it is automatically going to return the response in the https response body
+//when we have wrote the restcontroller its start collecting the response and request
 public class TodoController {
+    public static List<Todo> todos ;
+
+    public TodoController() {
+        todos=new ArrayList<>();
+        todos.add(new Todo(1,true,"MY",231));
+        todos.add(new Todo(2,false,"Ms",232));
+    }
 }
